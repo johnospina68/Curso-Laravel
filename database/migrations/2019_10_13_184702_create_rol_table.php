de0ca2e3dcee8f037bca4f3179e98e096d8debd7
PrimerCursoLaravel <johnospina68@gmail.com>
2019-10-13 17:47:51 -0500

    Trabajo con Seeder y Model Factory

--- database/migrations/2019_10_13_184702_create_rol_table.php
+++ database/migrations/2019_10_13_184702_create_rol_table.php
@@ -15,7 +15,7 @@ class CreateRolTable extends Migration
     {
         Schema::create('rol', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('nombre', 50);
+            $table->string('nombre', 50)->unique();
             $table->timestamps();
         });
     }
